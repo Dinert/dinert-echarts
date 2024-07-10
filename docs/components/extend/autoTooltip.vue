abc1c794c5244f0663c3b9aeb789b9085ed6620e
dinert <1293437362@qq.com>
2024-07-10 15:54:55 +0800

    fix: 优化自动播放tooltip

--- docs/components/extend/autoTooltip.vue
+++ docs/components/extend/autoTooltip.vue
@@ -5,26 +5,29 @@ import {ChartInstance, initCharts, setOption, useAutoPlayTooltip} from '../../..
 const chartDom = ref(null)
 const charts = ref<ChartInstance | null>(null)
 const useAutoPlay: any = ref({})
-onMounted(async () => {
-    const chartInstance = await initCharts(chartDom.value)
-    setOption(chartInstance, {
-        xAxis: {
-            type: 'category',
-            data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
-        },
-        tooltip: {},
-        yAxis: {
-            type: 'value'
-        },
-        series: [
-            {
-                data: [150, 230, 224, 218, 135, 147, 260],
-                type: 'line'
-            }
-        ]
+
+onMounted(() => {
+    initCharts(chartDom.value).then(chartInstance => {
+        setOption(chartInstance, {
+            xAxis: {
+                type: 'category',
+                data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+            },
+            tooltip: {},
+            yAxis: {
+                type: 'value'
+            },
+            series: [
+                {
+                    data: [150, 230, 224, 218, 135, 147, 260],
+                    type: 'line'
+                }
+            ]
+        })
+        useAutoPlay.value = useAutoPlayTooltip(chartInstance, 1000)
+        charts.value = chartInstance
     })
-    useAutoPlay.value = useAutoPlayTooltip(chartInstance, 1000)
-    charts.value = chartInstance
+
 })
 
 onBeforeUnmount(() => {
