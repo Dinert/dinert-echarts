d165b82112dae8f7b361ddeea971e462ff50edbb
dinert <1293437362@qq.com>
2024-07-11 10:43:15 +0800

    feat: x和y轴的优化

--- packages/setOption.ts
+++ packages/setOption.ts
@@ -1,39 +1,33 @@
 import {ChartInstance, ChartSetOptionsParams, RewriteChartSetOptionsParamsOptions} from './types/common'
 import lodash from 'lodash'
 
-const defaultOptions = {
-    tooltip: {},
-    grid: {
-        left: '3%',
-        right: '3%',
-        bottom: '3%',
-        containLabel: true
-    },
-}
-const changeOptions = (charts: ChartInstance, options: RewriteChartSetOptionsParamsOptions) => {
-    if (options && Object.prototype.toString.call(options.xAxis) === '[object Array]') {
-        options.xAxis?.forEach(item => {
-            item.axisTick = {show: false, ...item.axisTick}
-        })
-    } else if (options && Object.prototype.toString.call(options.xAxis) === '[object Object]') {
-        options.xAxis.axisTick = {show: false, ...options.xAxis?.axisTick}
+const defaultOptions = (charts: ChartInstance, options: RewriteChartSetOptionsParamsOptions): RewriteChartSetOptionsParamsOptions => {
+    const newOptions = {
+        tooltip: {},
+        grid: {
+            left: '3%',
+            right: '3%',
+            bottom: '3%',
+            containLabel: true
+        },
+        xAxis: {},
+        yAxis: {}
+    }
+    if (['line', 'bar', undefined].includes(options.type)) {
+        newOptions.xAxis = {axisTick: {show: false}}
+        newOptions.yAxis = {axisLine: {show: true}}
+    } else if (options.type === 'pie') {
+        delete (newOptions as any).xAxis
+        delete (newOptions as any).yAxis
     }
 
-    if (options && Object.prototype.toString.call(options.yAxis) === '[object Array]') {
-        options.yAxis?.forEach(item => {
-            item.axisLine = {show: true, ...item.axisLine}
-        })
-    } else if (options && Object.prototype.toString.call(options.yAxis) === '[object Object]') {
-        options.yAxis.axisLine = {show: true, ...options.yAxis?.axisLine}
-    }
+    return newOptions
 }
-
 // eslint-disable-next-line max-len
 export const setOption = (charts: ChartInstance, options: RewriteChartSetOptionsParamsOptions, notMerge?: ChartSetOptionsParams[1]): Promise<ChartInstance> => {
     return new Promise(resolve => {
 
-        changeOptions(charts, options)
-        const options2 = lodash.defaultsDeep(lodash.cloneDeep(options), defaultOptions)
+        const options2 = lodash.defaultsDeep(lodash.cloneDeep(options), defaultOptions(charts, options))
 
         const configOptions = typeof options.configBefore === 'function' && options.configBefore(charts, options2)
 
